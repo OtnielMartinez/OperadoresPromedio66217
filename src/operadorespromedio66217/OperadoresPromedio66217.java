da518e774f1bc6cb189d318d77d7a31c112dc9ae
lampi <lampi@192.168.1.78>
2021-03-02 07:38:38 -0600

    Commit para repocitorio

---
+++ src/operadorespromedio66217/OperadoresPromedio66217.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorespromedio66217;
+
+/**
+ *
+ * @author lampi
+ */
+public class OperadoresPromedio66217 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Promedio de 5 numeros
+        int valor1 = 8;
+        float valor2 = 8.5f;
+        float valor3 = 9.5f;
+        int valor4 = 10;
+        short valor5 = 7;
+        
+        float sumadevalores = valor1+valor2+valor3+valor4+valor5;
+        float promediodelosvalores = sumadevalores / 5;
+        
+        System. out. println("El promedio de los valores es = " + promediodelosvalores);
+    }
+    
+}
